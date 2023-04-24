a946a4c3a7c0e17ddd0470ed6fa2522cd2abbe41
Isaac Berlin <isaacmorrisberlin@gmail.com>
2023-04-23 23:23:24 -0500

    added bubble, selection, insertion, and merge

---
+++ src/algorithm.py
@@ -0,0 +1,62 @@
+def bubble_sort(arr):
+    nunmber_of_swaps = 0
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                nunmber_of_swaps += 1
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr, nunmber_of_swaps
+
+def selection_sort(arr):
+    number_of_swaps = 0
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        number_of_swaps += 1
+    return arr, number_of_swaps
+
+def insertion_sort(arr):
+    number_of_swaps = 0
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            number_of_swaps += 1
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr, number_of_swaps
+
+def merge_sort(arr):
+    number_of_swaps = 0
+    right_swaps = 0
+    left_swaps = 0
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        left_swaps = merge_sort(left)[1]
+        right_swaps = merge_sort(right)[1]
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            number_of_swaps += 1
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            number_of_swaps += 1
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr, (number_of_swaps+left_swaps+right_swaps)
